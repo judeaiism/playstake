efc630c0e410c69d220aa28faf6b91b3985791ae
judeaiism <judeaiism@outlook.com>
2024-09-14 14:25:16 +0000

    v6

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     domains: ['firebasestorage.googleapis.com'],
+  },
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+      ignored: ['**/node_modules', '!**/*.hot-update.js']
+    }
+    return config
   },
 }
 
